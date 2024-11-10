4c65f9ba7c6bb23b928c52713fa03106e0ca4187
Yubaraj Sarkar <41476315+Ninja-Yubaraj@users.noreply.github.com>
2024-11-10 18:11:39 +0530

    Initial Commit.

---
+++ script.js
@@ -0,0 +1,13 @@
+// Redirect links for each button (you can change the URLs later)
+document.querySelector('.spotify-button').addEventListener('click', () => {
+    window.location.href = 'https://open.spotify.com/playlist/1T9Pt0CEVLrFzzokp4Ognt?si=2ba35a54d2754a41';
+  });
+  
+  document.querySelector('.privacy-button').addEventListener('click', () => {
+    window.location.href = 'https://www.spotify.com/legal/privacy-policy/';
+  });
+  
+  document.querySelector('.powered-by').addEventListener('click', () => {
+    window.location.href = 'https://github.com/Ninja-Yubaraj';
+  });
+  
